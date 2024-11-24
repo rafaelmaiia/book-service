d3437424b7a543842b543365533233c59fd4fbf5
Rafael Maia <106180433+rafaelmaiia@users.noreply.github.com>
2024-11-24 16:31:07 -0300

    fixed problems with Jackson serialization

--- src/main/java/br/com/rafaelmaia/model/Book.java
+++ src/main/java/br/com/rafaelmaia/model/Book.java
@@ -4,17 +4,44 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
+@Entity(name = "book")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "author", nullable = false, length = 180)
 	private String author;
+	
+	@Column(name = "launch_date", nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date launchDate;
+	
+	@Column(nullable = false)
 	private Double price;
+	
+	@Column(name = "title", length = 250)
 	private String title;
+	
+	@Transient
 	private String currency;
+	
+	@Transient
 	private String environment;
 	
 	public Book() {
